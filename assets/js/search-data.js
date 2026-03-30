6feea4b9147a25af05aa0b85cf34b28f71ef6c69
dachengx <xudctron@gmail.com>
2026-03-30 05:22:16 +0000

    Deploying to gh-pages from @ dachengx/dachengx.github.io@b07e56bf9f7657250742177da4d25ad74b6fe455 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -391,16 +391,17 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+            },},{id: "news-started-to-work-as-an-analysis-tools-team-leader-in-xenonnt",
+          title: 'Started to work as an Analysis Tools Team Leader in XENONnT.',
           description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
+          section: "News",},{id: "news-first-indication-of-solar-b-neutrinos-via-coherent-elastic-neutrino-nucleus-scattering-with-xenonnt-is-published-in-prl-as-editors-suggestion",
+          title: '“First Indication of Solar ⁸B Neutrinos via Coherent Elastic Neutrino-Nucleus Scattering with XENONnT”...',
           description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          section: "News",},{id: "news-got-promoted-from-analysis-tools-team-leader-to-analysis-coordinator-of-xenonnt",
+          title: 'Got promoted from Analysis Tools Team Leader to Analysis Coordinator of XENONnT.',
+          description: "",
+          section: "News",},{id: "news-completed-term-as-xenonnt-analysis-coordinator",
+          title: 'Completed term as XENONnT Analysis Coordinator.',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
